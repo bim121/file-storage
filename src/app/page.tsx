57e7ceb2b707e19f5e3695c0756c03cd2eebe575
Vitalik <vitisik54@gmail.com>
2024-10-28 04:55:32 +0300

    users code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { SignInButton, SignedIn, SignedOut, SignOutButton } from "@clerk/nextjs";
+import { SignInButton, SignedIn, SignedOut, SignOutButton, useOrganization, useUser } from "@clerk/nextjs";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 
 export default function Home() {
+  const organization = useOrganization();
+  const user = useUser();
+
+  let orgId: string | undefined = undefined;
+  if(organization.isLoaded && user.isLoaded){
+    orgId = organization.organization?.id ?? user.user?.id;
+  }
+
   const createFile = useMutation(api.files.createFile);
-  const files = useQuery(api.files.getFiles);
+  const files = useQuery(api.files.getFiles, orgId ? {orgId} : "skip");
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-3">
@@ -28,8 +36,10 @@ export default function Home() {
 
       <Button
         onClick={() => {
+          if(!orgId) return;
           createFile({
-            name: "hellow world"
+            name: "hellow world",
+            orgId: orgId
           });
         }}
       >
